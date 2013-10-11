08ac4402825829dfcbb0e44a31de269a4faf1950
sasagichuki@gmail.com <sasagichuki@gmail.com>
2013-10-11 11:48:46 +0300

    New banner ana title font (Josefin Slab)

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 
 	<!-- stylesheet -->
 	<link rel="stylesheet" media="all" href="<?php bloginfo('stylesheet_url'); ?>"/>
-	<link href='http://fonts.googleapis.com/css?family=Candal|Open+Sans:100,300italic,400' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Josefin+Slab:400,600|Open+Sans:100,300italic,400' rel='stylesheet' type='text/css'>
 	<!-- stylesheet -->
 
 	
